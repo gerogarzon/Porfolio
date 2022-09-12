699cdfc24dba2124997c43af615cf6c5827dc150
gerogarzon <gerogarzon@gmail.com>
2022-09-12 13:34:39 -0300

    main changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,11 +7,12 @@ const Main = () => {
     <div className="main">
       <Header />
       <div className="body-main-container">
-        <h1 style={{ fontSize: "30px", margin: "0px" }}>Hello! I'm</h1>
-        <h1 style={{ fontSize: "70px", margin: "0px", color: "#15bb91" }}>
-          Gero
+        <h1  style={{ margin: "0px"}}>Hello World </h1>
+        <h1 style={{ fontSize: "70px", margin: "0px"}}>
+        I'm <b style={{ fontSize: "70px", margin: "0px", color: "#15bb91" }}>Gero</b>
         </h1>
         <h2>Full Stack Developer</h2>
+        <h5>Currently located in Argentina</h5>
        
         <br></br>
         <br></br>
